690a795aacb5d1651ec1f4e4d32eea52337f8233
samuel-yeebiyo <samoyemaneyy@gmail.com>
2024-03-19 16:29:04 +0000

    fix: updated frontend to fix pagination and some styling

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -7,34 +7,61 @@ import { Pagination } from "@mantine/core";
 
 const App = () => {
     const [page, setPage] = useState(1);
-    const { status, isFetched, data } = useQuery({
+    const { status, error, isFetching, data } = useQuery({
         queryKey: ["todos", page],
         queryFn: () => fetchPaginatedTodos(page),
     });
 
     return (
-        <div className="h-[100vh] bg-[#111827] text-white pt-5 flex flex-col items-center">
+        <div className="h-full text-white my-10 flex flex-col items-center">
             <h1 className="text-3xl font-bold">To-do List App</h1>
             <h2 className="mb-8">Revolutionizing task management!</h2>
             <div className="max-w-[550px] w-[90%] flex flex-col items-center">
-                <div>
-                    {isFetched &&
-                        status == "success" &&
-                        data.data.map((todo: TodoType) => (
-                            <Todo
-                                key={todo.id}
-                                id={todo.id}
-                                status={todo.status}
-                                title={todo.title}
-                            />
-                        ))}
+                <div className="w-full">
+                    {isFetching ? (
+                        !data ? (
+                            <div className="w-full text-lg grid place-content-center bg-[#1e2636] h-96 rounded-lg">
+                                Fetching...
+                            </div>
+                        ) : (
+                            data.data.map((todo: TodoType) => (
+                                <Todo
+                                    key={todo.id}
+                                    id={todo.id}
+                                    status={todo.status}
+                                    title={todo.title}
+                                />
+                            ))
+                        )
+                    ) : status == "success" ? (
+                        data.data.length == 0 ? (
+                            <div className="w-full text-lg grid place-content-center bg-[#1e2636] h-96 rounded-lg">
+                                No tasks to-do..yet
+                            </div>
+                        ) : (
+                            data.data.map((todo: TodoType) => (
+                                <Todo
+                                    key={todo.id}
+                                    id={todo.id}
+                                    status={todo.status}
+                                    title={todo.title}
+                                />
+                            ))
+                        )
+                    ) : (
+                        <div className="w-full text-lg grid place-content-center bg-[#1e2636] h-96 rounded-lg">
+                            {error?.message}
+                        </div>
+                    )}
                 </div>
-                <Pagination
-                    className="w-fit mt-5"
-                    value={data?.current_page}
-                    onChange={setPage}
-                    total={data?.last_page}
-                />
+                {status == "success" && data.data.length != 0 && (
+                    <Pagination
+                        className="w-fit mt-5"
+                        value={status == "success" ? data?.current_page : 0}
+                        onChange={setPage}
+                        total={status == "success" ? data?.last_page : 0}
+                    />
+                )}
             </div>
         </div>
     );
