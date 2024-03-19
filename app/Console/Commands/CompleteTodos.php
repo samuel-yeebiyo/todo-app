404aed1e8df0b5087187d1865c213224a813c79b
samuel-yeebiyo <samoyemaneyy@gmail.com>
2024-03-19 13:15:06 +0000

    feat: added artisan command for completing todo items using a queue system

---
+++ app/Console/Commands/CompleteTodos.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Models\Todo;
+use App\Jobs\CompleteTodoItems;
+
+class CompleteTodos extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'todo:complete-todos';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Complete all todo items';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Todo::where('status', 'pending')->chunk(100, function($chunk){
+            CompleteTodoItems::dispatch($chunk);
+        });
+    }
+}
---
+++ app/Jobs/CompleteTodoItems.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CompleteTodoItems implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+    * The number of times the job may be attempted.
+    *
+    * @var int
+    */
+    protected $tries = 3;
+
+    protected $todo_chunk = null;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($chunk)
+    {
+        $this->todo_chunk = $chunk;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        foreach($this->todo_chunk as $todo){
+            if($todo->status == 'pending'){
+                $todo->update([
+                    'status'=>'done'
+                ]);
+            }
+        }
+    }
+}
